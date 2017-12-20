c4125a6b63991897d75ae3cd0240fbecdc564d32
tinhte96 <vanthiennhat@gmail.com>
2017-12-20 22:44:44 +0100

    tcp receiver handler

---
+++ src/main/java/com/insa/network/handler/TCPReceiverHandler.java
@@ -0,0 +1,39 @@
+package com.insa.network.handler;
+
+import com.insa.Model.Peer;
+import com.insa.network.IncomingMessageListener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPReceiverHandler implements  Runnable{
+
+    ServerSocket serverSocket;
+    Socket chatSocket;
+    BufferedInputStream stream;
+    IncomingMessageListener incomingMessageListener;
+
+    public TCPReceiverHandler(IncomingMessageListener incomingMessageListener) throws IOException {
+        serverSocket = new ServerSocket(Peer.PORT_COMMUNICATION);
+        chatSocket = serverSocket.accept();
+        stream = new BufferedInputStream(chatSocket.getInputStream());
+        this.incomingMessageListener = incomingMessageListener;
+    }
+
+    @Override
+    public void run() {
+        try{
+            byte[] message = new byte[50];
+            int lenMessage = stream.read(message);
+            System.out.println(lenMessage);
+            incomingMessageListener.onNewIncomingMessage(message);
+
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
