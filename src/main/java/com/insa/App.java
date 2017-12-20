60111f4163e623a43a88abbb7e5c707873309031
tinhte96 <vanthiennhat@gmail.com>
2017-12-20 22:50:46 +0100

    init node, load login page

--- src/main/java/com/insa/App.java
+++ src/main/java/com/insa/App.java
@@ -1,13 +1,49 @@
 package com.insa;
 
+import com.insa.Model.Node;
+import com.insa.Model.Peer;
+import com.insa.ui.Login;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main (String[] args) throws Exception {
+
+        String ip = getAddressIP();
+
+
+        Node node = new Node(new Peer(ip));
+
+            Login login = new Login(node);
+            login.display();
+        }
+
+        public static String getAddressIP() throws SocketException {
+
+            Enumeration e = NetworkInterface.getNetworkInterfaces();
+            while (e.hasMoreElements()) {
+                NetworkInterface n = (NetworkInterface) e.nextElement();
+                Enumeration ee = n.getInetAddresses();
+                int j = 0;
+
+                while (ee.hasMoreElements()) {
+                    InetAddress i = (InetAddress) ee.nextElement();
+                    if(!i.isLoopbackAddress() && j == 1) {
+                        return i.getHostAddress();
+                    }
+                    j++;
+                }
+            }
+            return null;
+        }
+
     }
-}
+
