0c371b0d718209d6621f642373c59006eba81426
tinhte96 <vanthiennhat@gmail.com>
2017-12-20 17:45:26 +0100

    Peer model

---
+++ src/main/java/com/insa/Model/Peer.java
@@ -0,0 +1,52 @@
+package com.insa.Model;
+
+import java.net.*;
+
+public class Peer {
+
+    public final static int PORT_BROADCAST = 8888 ;
+    public final static int PORT = 6666 ;
+
+    private String pseudonyme;
+    private String host;
+    private int port;
+
+    public Peer(String host) throws UnknownHostException {
+        this.host = host;
+        this.port = PORT;
+    }
+
+    public Peer(String pseudonyme, String host) throws UnknownHostException {
+        this.pseudonyme = pseudonyme;
+        this.host = host;
+        this.port = PORT;
+    }
+
+    public Peer(String pseudonyme, String host, int port) throws UnknownHostException {
+        this.pseudonyme = pseudonyme;
+        this.host = host;
+        this.port = port;
+    }
+
+    public void setPseudonyme(String pseudonyme) {this.pseudonyme = pseudonyme;}
+
+    public String getPseudonyme() {
+        return pseudonyme;
+    }
+
+    public void setHost(String host){
+        this.host = host;
+    }
+
+    public String getHost(){
+        return this.host;
+    }
+
+    public int getPort(){
+        return this.port;
+    }
+
+    public String toString(){
+        return "id "+ this.pseudonyme + " ("+ this.host+ " : "+ this.PORT+ ")";
+    }
+}
