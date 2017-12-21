35728d84f6f71b9d170f57aed028b69e251e9da6
tinhte96 <vanthiennhat@gmail.com>
2017-12-21 16:40:08 +0100

    send personal info to all peers

---
+++ src/main/java/com/insa/Message/InfoHandler.java
@@ -0,0 +1,22 @@
+package com.insa.Message;
+
+import com.insa.model.Node;
+import com.insa.model.Peer;
+
+import java.net.UnknownHostException;
+
+public class InfoHandler implements Runnable {
+
+    private Node node;
+    private Peer peer;
+
+    public InfoHandler(Node node, String data, String ipAddress, int port) throws UnknownHostException {
+        this.node = node;
+        this.peer = new Peer(data,ipAddress,port);
+    }
+
+    @Override
+    public void run() {
+        this.node.updatePeersList(peer);
+    }
+}
