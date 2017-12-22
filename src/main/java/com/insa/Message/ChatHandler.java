d69bac2f075c667ac830328209f6d790c01e41fb
tinhte96 <vanthiennhat@gmail.com>
2017-12-22 19:13:06 +0100

    pop up a notification for a new conversation

--- src/main/java/com/insa/Message/ChatHandler.java
+++ src/main/java/com/insa/Message/ChatHandler.java
@@ -1,25 +1,41 @@
 package com.insa.Message;
 
 import com.insa.model.Node;
+import com.insa.model.Peer;
+import com.insa.ui.Chat;
+
+import javax.swing.*;
 
 public class ChatHandler implements Runnable {
 
     private Node node;
     private String message;
     private String ipAddress;
-    private int port;
 
-    public ChatHandler(Node node, String data, String ipAddress, int port){
+    public ChatHandler(Node node, String data, String ipAddress){
         this.node = node;
         this.message = data;
         this.ipAddress = ipAddress;
-        this.port = port;
     }
 
     @Override
     public void run() {
         System.out.println("CALL IN ChatHandler :\nI received : " + this.message + "\nfrom " + this.ipAddress+"\nThank you");
+
         //TODO update chat window
+        Chat chat = this.node.getChatWindowForPeer(ipAddress);
+
+        if (chat == null){
+            Peer peer = this.node.findPeerByIpAddress(ipAddress);
+            Chat chatd = new Chat(null,node,peer);
+            Object[] options = {chatd , "CANCEL" };
+            JOptionPane.showOptionDialog(null, "Click OK to open chat", "you have 1 new message",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+                    null, options, options[0]);
+        }
+        else{
+            chat.updateChatArea(this.message,false);
+        }
     }
 
 }
